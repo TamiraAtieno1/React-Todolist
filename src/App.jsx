7000f7d210a5e8308d7e80c09678de249b79688e
TamiraAtieno1 <tamiraatieno20@gmail.com>
2023-07-10 16:42:20 +0300

    created Todo

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Todolist from './components/todolist';
+import Todoitem from './components/todoitem';
+function App() {
+ 
+
+  return (
+    <>
+    <Todoitem/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/todoitem.jsx
@@ -0,0 +1,39 @@
+import React, {useState} from "react"
+import Todolist from "./todolist"
+import { v4 as uuidv4 } from 'uuid';
+import Todo from "./Todo";
+uuidv4()
+
+ const Todoitem = ()=>{
+    const [todos, setTodos] = useState([])
+
+    const addTodo = todo => {
+        setTodos([...todos, {id: uuidv4(), task: todo, completed: false, isEditing:false}])
+        console.log(todos)
+    }
+
+    const toggleComplete = id =>{
+        setTodos(todos.map(todo=> todo.id === id ? {...todo, completed: !todo.completed} :todo))
+    }
+
+    const deleteTodo = id =>{
+        setTodos(todos.filter(todo => todo.id !== id))
+    }
+
+    
+
+    return(
+        <div>
+            <Todolist addTodo={addTodo} />
+            {todos.map((todo, index) =>(
+                <Todo task={todo} key={index}
+                toggleComplete={toggleComplete}
+                deleteTodo={deleteTodo} />
+                
+            ))}
+        </div>
+    )
+
+}
+export default Todoitem;
+
